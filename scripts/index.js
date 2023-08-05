4178222a0edcb584619a1271643883c7de02b0b0
Wesley Chappell <chappellwesley@gmail.com>
2023-08-04 22:25:01 -0400

    name fade in

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 window.onload = () => {
     console.log('script loaded');
+    const title = document.getElementsByTagName('h1')[0];
+
+    const fadeIn = function (element) {
+        element.classList.remove('faded-right');
+    }
+
+    fadeIn(title);
 }
