6c5c232f3d1ffdf7172bd1a715284ad8eb46d05e
nukambe <chappellwesley@gmail.com>
2023-12-04 20:35:04 -0500

    scratch and add tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,17 @@
 <script>
-  // import svelteLogo from "./assets/svelte.svg";
-  import Navigation from "./lib/navigation/Navigation.svelte";
-  import ContentSection from "./lib/ContentSection.svelte";
-  import Home from "./lib/Home.svelte";
-  import About from "./lib/About.svelte";
-  import Work from "./lib/Work.svelte";
-  import Contact from "./lib/Contact.svelte";
-  import MobileNavigation from "./lib/navigation/MobileNavigation.svelte";
-  import Footer from "./lib/footer.svelte";
+
 </script>
 
+<header>
+header
+</header>
 <main>
-  <Navigation />
-  <div>
-    <MobileNavigation />
-    <ContentSection id="home"><Home /></ContentSection>
-    <ContentSection id="about"><About /></ContentSection>
-    <ContentSection id="work"><Work /></ContentSection>
-    <ContentSection id="contact"><Contact /></ContentSection>
-    <Footer />
-  </div>
+main
 </main>
+<footer>
+footer
+</footer>
 
-<style lang="less">
-  main {
-    display: flex;
-    overflow-y: hidden;
-  }
-  div {
-    width: 100%;
-    height: 100vh;
-    overflow-y: scroll;
-    scroll-behavior: smooth;
-  }
-  div::-webkit-scrollbar {
-    display: none;
-  }
+<style>
+
 </style>
