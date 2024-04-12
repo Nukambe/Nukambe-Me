82d77e03d78cdfd69bd04b8431c2614c2b7877f8
nukambe <chappellwesley@gmail.com>
2024-04-11 20:12:25 -0400

    frontend init

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,12 +4,14 @@ const certificationsRouter = require("./certifications.js");
 const profileRouter = require("./profile.js");
 const projectsRouter = require("./projects.js");
 const skillsRouter = require("./skills.js");
+const testimonialsRouter = require("./testimonials.js");
 
 router.use("/session", sessionRouter);
 router.use("/certifications", certificationsRouter);
 router.use("/profile", profileRouter);
 router.use("/projects", projectsRouter);
 router.use("/skills", skillsRouter);
+router.use("/testimonials", testimonialsRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
